6711994b5c2399e3ce04b98500feccab20c9da86
Estefania Vago <ecvago@hotmail.com>
2024-10-10 12:11:36 -0300

    cambios login

--- ClubDeportivo-DSOO-PI/Form1.Designer.cs
+++ ClubDeportivo-DSOO-PI/Form1.Designer.cs
@@ -28,11 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.TxtUsuario = new System.Windows.Forms.TextBox();
             this.Txtcontrasenia = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.LogoClub = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoClub)).BeginInit();
             this.SuspendLayout();
             // 
             // TxtUsuario
@@ -54,34 +55,37 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(437, 272);
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(518, 262);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(123, 44);
             this.button1.TabIndex = 2;
             this.button1.Text = "INGRESAR";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
-            // pictureBox1
+            // LogoClub
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(82, 117);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(241, 99);
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
+            this.LogoClub.Image = ((System.Drawing.Image)(resources.GetObject("LogoClub.Image")));
+            this.LogoClub.Location = new System.Drawing.Point(82, 74);
+            this.LogoClub.Name = "LogoClub";
+            this.LogoClub.Size = new System.Drawing.Size(298, 242);
+            this.LogoClub.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.LogoClub.TabIndex = 3;
+            this.LogoClub.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.LogoClub);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.Txtcontrasenia);
             this.Controls.Add(this.TxtUsuario);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoClub)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,7 +96,7 @@
         private System.Windows.Forms.TextBox TxtUsuario;
         private System.Windows.Forms.TextBox Txtcontrasenia;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox LogoClub;
     }
 }
 
