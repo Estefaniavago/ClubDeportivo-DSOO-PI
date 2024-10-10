0486fbb2b0e7b70040c5dc8293a449cd9da6ea40
Estefania Vago <ecvago@hotmail.com>
2024-10-10 08:53:36 -0300

    Primer commit

--- ClubDeportivo-DSOO-PI/Form1.Designer.cs
+++ ClubDeportivo-DSOO-PI/Form1.Designer.cs
@@ -28,13 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.TxtUsuario = new System.Windows.Forms.TextBox();
+            this.Txtcontrasenia = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TxtUsuario
+            // 
+            this.TxtUsuario.Location = new System.Drawing.Point(424, 117);
+            this.TxtUsuario.Margin = new System.Windows.Forms.Padding(5);
+            this.TxtUsuario.Name = "TxtUsuario";
+            this.TxtUsuario.Size = new System.Drawing.Size(285, 20);
+            this.TxtUsuario.TabIndex = 0;
+            this.TxtUsuario.Text = "USUARIO";
+            // 
+            // Txtcontrasenia
+            // 
+            this.Txtcontrasenia.Location = new System.Drawing.Point(424, 188);
+            this.Txtcontrasenia.Name = "Txtcontrasenia";
+            this.Txtcontrasenia.Size = new System.Drawing.Size(285, 20);
+            this.Txtcontrasenia.TabIndex = 1;
+            this.Txtcontrasenia.Text = "CONTRASEÑA";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(437, 272);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "INGRESAR";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(82, 117);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(241, 99);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Txtcontrasenia);
+            this.Controls.Add(this.TxtUsuario);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox TxtUsuario;
+        private System.Windows.Forms.TextBox Txtcontrasenia;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- ClubDeportivo-DSOO-PI/Form1.cs
+++ ClubDeportivo-DSOO-PI/Form1.cs
@@ -16,5 +16,10 @@ namespace ClubDeportivo_DSOO_PI
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
