e573aeae2d9846c776c9ee155dc5d7a09d9ccc88
Estefania Vago <ecvago@hotmail.com>
2024-10-30 09:21:01 -0300

    FORM2

--- ClubDeportivo-DSOO-PI/Form2.Designer.cs
+++ ClubDeportivo-DSOO-PI/Form2.Designer.cs
@@ -30,6 +30,11 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnRegistro = new System.Windows.Forms.Button();
+            this.btnPago = new System.Windows.Forms.Button();
+            this.btnCarnet = new System.Windows.Forms.Button();
+            this.btnListado = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,7 +45,7 @@
             this.label1.Font = new System.Drawing.Font("Cambria", 24F, System.Drawing.FontStyle.Bold);
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(57)))), ((int)(((byte)(80)))));
             this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.label1.Location = new System.Drawing.Point(266, 83);
+            this.label1.Location = new System.Drawing.Point(236, 74);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(272, 37);
@@ -53,13 +58,70 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = global::ClubDeportivo_DSOO_PI.Properties.Resources.fitmoveRecurso_2_3x;
-            this.pictureBox1.Location = new System.Drawing.Point(328, 20);
+            this.pictureBox1.Location = new System.Drawing.Point(299, 11);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(128, 61);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
+            // 
+            // btnRegistro
+            // 
+            this.btnRegistro.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnRegistro.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistro.Location = new System.Drawing.Point(92, 125);
+            this.btnRegistro.Name = "btnRegistro";
+            this.btnRegistro.Size = new System.Drawing.Size(185, 66);
+            this.btnRegistro.TabIndex = 7;
+            this.btnRegistro.Text = "REGISTRAR USUARIO";
+            this.btnRegistro.UseVisualStyleBackColor = false;
+            // 
+            // btnPago
+            // 
+            this.btnPago.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnPago.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPago.Location = new System.Drawing.Point(393, 125);
+            this.btnPago.Name = "btnPago";
+            this.btnPago.Size = new System.Drawing.Size(185, 66);
+            this.btnPago.TabIndex = 8;
+            this.btnPago.Text = "PAGO";
+            this.btnPago.UseVisualStyleBackColor = false;
+            this.btnPago.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCarnet
+            // 
+            this.btnCarnet.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnCarnet.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCarnet.Location = new System.Drawing.Point(92, 221);
+            this.btnCarnet.Name = "btnCarnet";
+            this.btnCarnet.Size = new System.Drawing.Size(185, 66);
+            this.btnCarnet.TabIndex = 9;
+            this.btnCarnet.Text = "EMITIR CARNET";
+            this.btnCarnet.UseVisualStyleBackColor = false;
+            // 
+            // btnListado
+            // 
+            this.btnListado.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnListado.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListado.Location = new System.Drawing.Point(393, 221);
+            this.btnListado.Name = "btnListado";
+            this.btnListado.Size = new System.Drawing.Size(185, 66);
+            this.btnListado.TabIndex = 10;
+            this.btnListado.Text = "VENCIMIENTOS";
+            this.btnListado.UseVisualStyleBackColor = false;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(243, 312);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(185, 66);
+            this.btnSalir.TabIndex = 11;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form2
             // 
@@ -68,9 +130,14 @@
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(688, 415);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnListado);
+            this.Controls.Add(this.btnCarnet);
+            this.Controls.Add(this.btnPago);
+            this.Controls.Add(this.btnRegistro);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu Principal";
@@ -85,5 +152,10 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnRegistro;
+        private System.Windows.Forms.Button btnPago;
+        private System.Windows.Forms.Button btnCarnet;
+        private System.Windows.Forms.Button btnListado;
+        private System.Windows.Forms.Button btnSalir;
     }
 }
--- ClubDeportivo-DSOO-PI/Form2.cs
+++ ClubDeportivo-DSOO-PI/Form2.cs
@@ -36,95 +36,7 @@ namespace ClubDeportivo_DSOO_PI
 
         private void Form2_Load(object sender, EventArgs e)
         {
-
           
-
-            int buttonWidth = 200;
-            int buttonHeight = 50;
-            int horizontalSpacing = 80; // Espacio horizontal entre botones
-            int verticalSpacing = 40; // Espacio vertical entre botones
-            
-
-            // Calculo la posición para poder centrar los botones
-            int startX = (this.ClientSize.Width - buttonWidth * 2 - horizontalSpacing) / 2;
-            int startY = (this.ClientSize.Height - buttonHeight * 2 - verticalSpacing) / 2;
-
-            // Configuración del botón "REGISTRO USUARIO"
-            CustomButton btnAltaSocio = new CustomButton();
-            btnAltaSocio.Text = "REGISTRO USUARIO";
-            btnAltaSocio.Size = new Size(buttonWidth, buttonHeight);
-            btnAltaSocio.Location = new Point(startX, startY+10); 
-            btnAltaSocio.BackColor = Color.FromArgb(12, 57, 80);
-            btnAltaSocio.ForeColor = Color.White;
-            btnAltaSocio.Click += BtnAltaSocio_Click;
-            this.Controls.Add(btnAltaSocio);
-
-            // Configuración del botón "EMITIR CARNET"
-            CustomButton btnEmitirCarnet = new CustomButton();
-            btnEmitirCarnet.Text = "EMITIR CARNET";
-            btnEmitirCarnet.Size = new Size(buttonWidth, buttonHeight);
-            btnEmitirCarnet.Location = new Point(startX + buttonWidth + horizontalSpacing, startY+10); 
-            btnEmitirCarnet.BackColor = Color.FromArgb(12, 57, 80);
-            btnEmitirCarnet.ForeColor = Color.White;
-            btnEmitirCarnet.Click += BtnEmitirCarnet_Click;
-            this.Controls.Add(btnEmitirCarnet);
-
-            // Configuración del botón "LISTADO DE VENCIMIENTO"
-            CustomButton btnListadoDeVencimiento = new CustomButton();
-            btnListadoDeVencimiento.Text = "VENCIMIENTOS";
-            btnListadoDeVencimiento.Size = new Size(buttonWidth, buttonHeight);
-            btnListadoDeVencimiento.Location = new Point(startX, startY + buttonHeight + verticalSpacing); // Nueva fila
-            btnListadoDeVencimiento.BackColor = Color.FromArgb(12, 57, 80);
-            btnListadoDeVencimiento.ForeColor = Color.White;
-            btnListadoDeVencimiento.Click += BtnListadoDeVencimiento_Click;
-            this.Controls.Add(btnListadoDeVencimiento);
-
-            // Configuración del botón "PAGO"
-            CustomButton btnPagoCuota = new CustomButton();
-            btnPagoCuota.Text = "PAGO DE CUOTA";
-            btnPagoCuota.Size = new Size(buttonWidth, buttonHeight);
-            btnPagoCuota.Location = new Point(startX + buttonWidth + horizontalSpacing, startY + buttonHeight + verticalSpacing); 
-            btnPagoCuota.BackColor = Color.FromArgb(12, 57, 80);
-            btnPagoCuota.ForeColor = Color.White;
-            btnPagoCuota.Click += BtnPagoCuota_Click;
-            this.Controls.Add(btnPagoCuota);
-
-            // Configuración del botón "Volver, regresa a la pantalla de login"
-            CustomButton btnSalir = new CustomButton();
-            btnSalir.Text = "VOLVER";
-            btnSalir.Size = new Size(buttonWidth, buttonHeight);
-            btnSalir.Location = new Point(290,300); 
-            btnSalir.BackColor = Color.FromArgb(12, 57, 80);
-            btnSalir.ForeColor = Color.White;
-            btnSalir.Click += BtnSalir_Click;
-            this.Controls.Add(btnSalir);
-        }
-
-        private void BtnAltaSocio_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Registro de usuario presionado.");
-        }
-
-        private void BtnEmitirCarnet_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Emitir carnet presionado.");
-        }
-
-        private void BtnListadoDeVencimiento_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Vencimientos presionado.");
-        }
-
-        private void BtnPagoCuota_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Pago Cuota presionado.");
-        }
-
-        private void BtnSalir_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show($"Hasta luego, {nombreUsuario}");
-            this.Close(); // Cierra el formulario
-            
             
         }
 
@@ -134,6 +46,16 @@ namespace ClubDeportivo_DSOO_PI
         }
 
         private void Form2_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
         {
 
         }
