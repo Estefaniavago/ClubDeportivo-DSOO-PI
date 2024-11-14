c468b4e8f322f5c196081b217a0cb11a1e8f3f29
Vanesa Soria <106666059+Vane-SDev@users.noreply.github.com>
2024-11-14 09:29:39 -0300

    Update ProyectoIntegrador.sql

--- ProyectoIntegrador.sql
+++ ProyectoIntegrador.sql
@@ -11,6 +11,8 @@ Activo boolean default true,
 constraint pk_usuario primary key (idUsuario)
 );
 
+
+
 drop table if exists persona;
 create table if not exists persona(
 idRegistro int,
@@ -23,14 +25,43 @@ condicion boolean default true,
 constraint pk_persona primary key (idRegistro)
 
 ); 
-drop table if exists vencimientos;
-create table if not exists vencimientos(
-idPago int auto_increment,
-idRegistro int,
-fecha DATE,
-constraint pk_vencimientos primary key (idPago),
-foreign key (idRegistro) references persona(idRegistro)
+
+CREATE TABLE vencimientos(
+    idPago INT AUTO_INCREMENT,
+    idRegistro INT,
+    fechaPago DATE,
+    fechaVencimiento DATE,
+    medioPago VARCHAR(20),
+    cuotas INT,
+    CONSTRAINT pk_vencimientos PRIMARY KEY (idPago),
+    FOREIGN KEY (idRegistro) REFERENCES persona(idRegistro)
 );
+
+create table actividad(
+NActividad int,
+Nombre varchar(40),
+precio float,
+constraint pk_actividad primary key(NActividad)
+);
+
+insert into actividad values
+(1000,"Fútbol",5200),
+(1001,"Spinning",5000),
+(1002,"Crossfit",5000),
+(1003,"Natación",5500),
+(1004,"Pilates",5000),
+(1005,"Musculación",4000),
+(1006,"Tenis",5000),
+(1007,"Boxeo",4000),
+(1008,"Taekwondo",4000),
+(1009,"Hockey",5000),
+(1010,"Gimnasia Rítmica",4000),
+(1011,"Gimnasia Artistica",4000),
+(1012,"Futsal",5000),
+(1013,"Basquet",5000),
+(1014,"Volley",5000),
+(1015,"Handball",5000),
+(1016,"Atletismo",5500);
 
 insert into usuario(idUsuario,nombreUsuario,claveUsuario) values
 (1,'Vanesa','1234'),
@@ -83,11 +114,20 @@ si los datos de los parametros NO EXISTEN la consulta arroja 0 FILAS
      end if;
 	 
 	  if existe = 0 then	 
-		 insert into persona values(filas,nom,ape,tipo,doc,1,1);
+		 insert into persona values(filas,nom,ape,tipo,doc,apto,cond);
 		 set res  = filas;
 	  else
 		 set res = existe;
-      end if;		 
+      end if;	
+      
 	end //
+    
+    DELIMITER //
+CREATE PROCEDURE EliminarPersona(IN id INT)
+BEGIN
+    DELETE FROM persona WHERE idRegistro = id;
+END //
+     DELIMITER ;
 
-select * from persona
+select * from persona;
+
